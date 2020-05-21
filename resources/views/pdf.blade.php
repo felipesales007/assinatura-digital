989d4278340cf81f421e43d14b77b334314e1ed4
Felipe Sales <felipesales007@hotmail.com>
2020-05-21 18:31:58 -0300

    criação da página do PDF

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Assinatura Digital</title>
+    </head>
+    <body>
+        <div>
+            Exemplo de assinatura digital.
+            <br>
+            by: Felipe Sales
+            <br>
+            <a href="https://www.felipesales.com.br">https://www.felipesales.com.br</a>
+        </div>
+    </body>
+</html>
