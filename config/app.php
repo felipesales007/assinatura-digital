ea4e881fe2956ede1fbf3bb2c9809d20253606e3
Felipe Sales <felipesales007@hotmail.com>
2020-05-21 18:36:58 -0300

    instalação do pacote TCPDF com o comando: composer require elibyy/tcpdf-laravel

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Elibyy\TCPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Elibyy\TCPDF\Facades\TCPDF::class
 
     ],
 
