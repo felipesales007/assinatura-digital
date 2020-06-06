04b1395ec611851fe6fd8a341e2d613955f495fa
Wanderley Ferreira de Albuquerque <wanderleyfa@gmail.com>
2020-06-06 12:09:41 -0300

    - minor updates - preparing for unit tests

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
